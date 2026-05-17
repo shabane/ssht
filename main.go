5579dcdb09049e611f50b1e4de274b790ce99608
shabane <m.mohamadshabane@gmail.com>
2026-05-18 00:35:22 +0330

    feat: use other functions to build UI and other stuff

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"ssht/component"
 	"ssht/sshUtils"
+
+	"github.com/rivo/tview"
 )
 
 func main() {
-	sshUtils.GetAllHosts()
+	app := tview.NewApplication()
+
+	mainBox := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(component.SearchBox, 3, 0, true).
+		AddItem(component.ListBox, 0, 1, false)
+
+	for _, host := range sshUtils.GetAllHosts() {
+		component.ListBox.AddItem(host, "", 0, nil)
+	}
+
+	err := app.SetRoot(mainBox, true).SetFocus(component.SearchBox).EnableMouse(true).Run()
+	if err != nil {
+		panic(err)
+	}
 }

fffca00515ac93450d497d3b76ba6800aeb5d472
shabane <m.mohamadshabane@gmail.com>
2026-05-18 15:07:07 +0330

    feat: set the foucuses on boxes

--- main.go
+++ main.go
@@ -40,12 +40,18 @@ func main() {
 		} else if keyName == "Ctrl+G" {
 			component.ListBox.Clear()
 			component.ListBox.AddItem("↑↓", "Up/Down The list", 0, nil)
-			component.ListBox.AddItem("Esc", "Show Hosts", 0, nil)
+			component.ListBox.AddItem("Esc", "Back", 0, nil)
 			component.ListBox.AddItem("Enter", "Open Selected Host", 0, nil)
 			component.ListBox.AddItem("CTRL+A", "Connect To All Filtered Hosts In Tailed Mode", 0, nil)
 			component.ListBox.AddItem("CTRL+O", "Connect To All Filtered Hosts In Synchronized Pane", 0, nil)
 			component.ListBox.AddItem("CTRL+N", "Connect To All Filtered Hosts Eeach In New Window", 0, nil)
 			component.ListBox.AddItem("CTRL+C", "Quit", 0, nil)
+		} else if keyName == "Down" {
+			tvewUtils.App.SetFocus(component.ListBox)
+		} else if keyName == "Up" {
+			tvewUtils.App.SetFocus(component.ListBox)
+		} else {
+			tvewUtils.App.SetFocus(component.SearchBox)
 		}
 
 		return event

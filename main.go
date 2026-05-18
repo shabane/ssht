dcf1cd87bc64cc974a7ace0e2a0837867348691f
shabane <m.mohamadshabane@gmail.com>
2026-05-18 14:58:45 +0330

    feat: add help in code

--- main.go
+++ main.go
@@ -24,7 +24,28 @@ func main() {
 	tvewUtils.MainBox.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		keyName := event.Name()
 		if keyName == "Ctrl+A" {
+			//TODO: fix before seach
+			tmuxUtils.OpenSelectedInTmux(tmuxUtils.TailedPane)
+		} else if keyName == "Ctrl+O" {
 			tmuxUtils.OpenSelectedInTmux(tmuxUtils.SyncedTailedPane)
+		} else if keyName == "Ctrl+N" {
+			tmuxUtils.OpenSelectedInTmux(tmuxUtils.Window)
+		} else if keyName == "Esc" {
+			component.ListBox.Clear()
+			for _, host := range sshUtils.AllHosts {
+				component.ListBox.AddItem(host, "", 0, func() {
+					tmuxUtils.OpenOneInTmux(host)
+				})
+			}
+		} else if keyName == "Ctrl+G" {
+			component.ListBox.Clear()
+			component.ListBox.AddItem("↑↓", "Up/Down The list", 0, nil)
+			component.ListBox.AddItem("Esc", "Show Hosts", 0, nil)
+			component.ListBox.AddItem("Enter", "Open Selected Host", 0, nil)
+			component.ListBox.AddItem("CTRL+A", "Connect To All Filtered Hosts In Tailed Mode", 0, nil)
+			component.ListBox.AddItem("CTRL+O", "Connect To All Filtered Hosts In Synchronized Pane", 0, nil)
+			component.ListBox.AddItem("CTRL+N", "Connect To All Filtered Hosts Eeach In New Window", 0, nil)
+			component.ListBox.AddItem("CTRL+C", "Quit", 0, nil)
 		}
 
 		return event
